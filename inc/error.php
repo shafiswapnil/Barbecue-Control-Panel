0f6c577dc0a9a83804f265a66f8d9da5229297f3
Shafi Swapnil <swapnil.env@gmail.com>
2023-02-19 18:37:55 +0600

    errpr php script

---
+++ inc/error.php
@@ -0,0 +1,28 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Error!</title>
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="text-center mx-auto mt-3 mb-3" style="max-width: 18rem;">
+				<div class="alert alert-danger" role="alert">
+					<strong>Error!</strong> in processing request
+				</div>
+				<div class="card border-danger">
+					<div class="card-body text-danger">
+						<h2 class="card-title">Stop!</h2>
+						<p class="card-text">There's an Internal Problem.</p>
+						<a href="../index.html" class="btn btn-danger">Go Back</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="../js/jquery-3.3.1.slim.min.js"></script>
+		<script src="../js/popper.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+	</body>
+</html>
