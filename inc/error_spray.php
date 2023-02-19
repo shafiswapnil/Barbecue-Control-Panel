fa199f02cb273e313dbf4226976315eb1b71748a
Shafi Swapnil <swapnil.env@gmail.com>
2023-02-19 18:38:13 +0600

    error spray php script

---
+++ inc/error_spray.php
@@ -0,0 +1,27 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Error!</title>
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="text-center mx-auto mt-3 mb-3" style="max-width: 28rem;">
+				<div class="alert alert-warning" role="alert">
+					<strong>Warning!</strong><br> You didn't select any Spray Option.
+				</div>
+				<div class="card border-info">
+					<div class="card-body text-info">
+						<p class="card-text">Please <strong>Go Back</strong> and turn on at least one option from spray section.</p>
+						<a href="../index.html" class="btn btn-info">Go Back</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="../js/jquery-3.3.1.slim.min.js"></script>
+		<script src="../js/popper.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+	</body>
+</html>
